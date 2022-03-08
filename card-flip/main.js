abff80f4ee4e6f13fb1289b76a592597a5c62b5e
Jay Franks <jay@healthystuff.com>
2022-03-08 14:24:14 +0000

    Removed unecessary files

--- card-flip/main.js
+++ card-flip/main.js
@@ -1,9 +1,3 @@
-// const card = document.querySelector(".card__inner");
-//
-// card.addEventListener("click", function (e) {
-//   card.classList.toggle('is-flipped');
-// });
-
 // Multiple cards
 const card = document.querySelectorAll(".card__inner");
 
